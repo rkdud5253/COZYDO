07f939a03bd19410adeb3818a3a1b6441761d153
신가영 <rkdud5253@naver.com>
2021-01-19 15:09:14 +0900

    Cozydo frontend initial code upload

---
+++ front-end/src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Main">
+    <h1>Main</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Main",
+  components: {
+    // HelloWorld
+  }
+};
+</script>
