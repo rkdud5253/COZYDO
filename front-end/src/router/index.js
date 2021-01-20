23e3008eb7a08255d6c6ef99983869a838cad95e
신다정 <ssdj0607@naver.com>
2021-01-20 13:52:02 +0900

    모바일웹 크기 초기 설정

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Main from "../views/Main.vue";
-
+import SearchResultList from "../views/search/SearchResultList.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -24,6 +24,11 @@ const routes = [
     name: "About2",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About2.vue")
+  },
+  {
+    path: "/searchResultList",
+    name: "SearchResultList",
+    component: SearchResultList
   }
 ];
 
---
+++ front-end/src/views/search/SearchResultList.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-btn color="success">success</v-btn>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchResultList',
+  component: {
+
+  },
+}
+</script>
