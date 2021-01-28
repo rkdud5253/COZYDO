8701ef23ceb6ca349ec902121d7f9ce32d546543
신다정 <ssdj0607@naver.com>
2021-01-28 18:31:27 +0900

    S04P12A201-67 S04P12A201-66 검색결과

--- front-end/src/components/SearchMap.vue
+++ front-end/src/components/SearchMap.vue
@@ -2,36 +2,32 @@
   <v-app>
       <!-- 지도 -->
       <div class="map pa-4" id="map"></div>
-      
+      {{ onChangeMap(value) }}
   </v-app>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: 'SearchMap',
   data: function() {
     return {
       map: '',
-      initPos: { lat: '37.5665734', lng: '126.978179' },
-      houseLat: '',
-      houseLng: '', // 좌표값 입력 받기
-      dong: '',
-      aptName: '',
-      ps: '',
-      // markers: [],
-      selected: '',
-      // infowindow: '',
-      imageSrc:
-        'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
-      imageSize: new kakao.maps.Size(24, 35),
-      // locPosition: '',
-      message: '',
+      initPos: { lat: '37.4942585', lng: '126.884217' },
+      items: [
+      ],
+      keyword: this.$route.query.keyword,
+      lat: 37.5031784,
+      lon: 126.8798427,
+      centerLat: 37.4968436,
+      centerLng: 127.0328341,
     };
   },
+  props: ['value'],
   mounted() {
     window.kakao && window.kakao.maps
       ? this.initMap()
-      : this.addKakaoMapScript();
+      : this.addScript();
   },
   methods: {
     initMap() {
@@ -42,38 +38,64 @@ export default {
       };
       this.map = new kakao.maps.Map(container, options);
       
+      // 현재 위치 마커 추가
       if (navigator.geolocation) {
+          this.nowlocation(this.map);
+      }
+          
+      // this.addMarker();
+      var zoomControl = new kakao.maps.ZoomControl();
+      this.map.addControl(zoomControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
     
-          // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    },
+    nowlocation(map) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
           navigator.geolocation.getCurrentPosition(function(position) {
-              
               var lat = position.coords.latitude, // 위도
                   lon = position.coords.longitude; // 경도
               
-             
+              console.log(typeof(lat));
               var markerPosition  = new kakao.maps.LatLng(lat, lon); 
-              console.log(markerPosition);
-              // 마커를 생성합니다
-              var marker = new kakao.maps.Marker({
+              // console.log(markerPosition);
+            
+               var marker = new kakao.maps.Marker({
+                  map: map,
                   position: markerPosition
               });
-
-              var container = document.getElementById('map');
-              var options = {
-                center: new kakao.maps.LatLng(lat, lon),
-                level: 7,
-              };
-              var map = new kakao.maps.Map(container, options);
               // // 마커가 지도 위에 표시되도록 설정합니다
               marker.setMap(map);
             });
-          
-      }
-
-      var zoomControl = new kakao.maps.ZoomControl();
-      this.map.addControl(zoomControl, kakao.maps.ControlPosition.BOTTOMRIGHT);
     },
+    addMarker(lat, lon) {
+      // 검색결과 마커 추가
+          var markers = [];
+          
+          var moveLatLon = new kakao.maps.LatLng(lat, lon);
     
+          // 지도 중심을 이동 시킵니다
+          this.map.setCenter(moveLatLon);
+          for(var i=0; i< 10; i++){
+              var tmpLat = this.items[i].placeLat;
+              var tmpLng = this.items[i].placeLon;
+              tmpLat = Number(tmpLat);
+              tmpLng = Number(tmpLng);
+
+              console.log(tmpLat + " " + tmpLng);
+              console.log(typeof(tmpLat));
+              var markerPosition  = new kakao.maps.LatLng(tmpLat, tmpLng); 
+             
+            
+               var marker = new kakao.maps.Marker({
+                  map: this.map,
+                  position: markerPosition,
+                  title: '새로운마커'
+              });
+              markers.push(marker)
+              // // 마커가 지도 위에 표시되도록 설정합니다
+              markers[i].setMap(this.map);
+          }
+          
+    },
     addScript() {
       const script = document.createElement('script');
       /* global kakao */
@@ -83,7 +105,64 @@ export default {
         'http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=6489698d36bee11ff3b7992ee0a7981a';
       document.head.appendChild(script);
     },
+    onChangeMap(value){
+      if(value=='mapcenter'){
+          axios.get('http://i4a201.p.ssafy.io:8080/map/list', {
+          params: {
+            keyword: this.keyword,
+            lat: this.centerLat,
+            lon: this.centerLng
+          }
+        })
+        .then((res) => {
+          // console.log(res);
+          this.items = res.data;
+          console.log(this.items)
+          // this.addMarker();
+          this.addMarker(this.centerLat, this.centerLng);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      }
+      else if(value=='mylocation'){
+         axios.get('http://i4a201.p.ssafy.io:8080/map/list', {
+          params: {
+            keyword: this.keyword,
+            lat: this.lat,
+            lon: this.lon
+          }
+        })
+        .then((res) => {
+          // console.log(res);
+          this.items = res.data;
+          console.log(this.items)
+          // this.addMarker();
+          this.addMarker(this.lat, this.lon);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+      }
+    }
   },
+  // created() {
+  //   axios.get('http://i4a201.p.ssafy.io:8080/map/list', {
+  //     params: {
+  //       keyword: this.keyword,
+  //       lat: 37.5019530,
+  //       lon: 127.039651
+  //     }
+  //   })
+  //   .then((res) => {
+  //     // console.log(res);
+  //     this.items = res.data;
+  //     this.addMarker();
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+  // }
 };
 </script>
 <style scoped>
