460710b689114e4226cb9d78f52f5b7872667aa4
wldbs_1120 <wldbs_1120@naver.com>
2021-02-07 02:53:46 +0900

    [front] 로그인 기능 수정중

--- front-end/src/views/member/kakaoLogin.vue
+++
@@ -1,9 +0,0 @@
-
-<template>
-    
-</template>
-
-<script type="text/javascript" src="https://developers.kakao.com/sdk/js/kakao.min.js">
-
-window.Kakao.init("9179c8a22ecad756bf861da78e37a03c")
-</script>
