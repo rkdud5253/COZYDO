e37b595279a500561f836e388b1aba098cd77c51
신가영 <rkdud5253@naver.com>
2021-02-08 11:45:41 +0900

    #S04P13A201-82 마이페이지 레이아웃 및 맵수정

--- front-end/src/components/mypage/MyPageReview.vue
+++ front-end/src/components/mypage/MyPageReview.vue
@@ -1,6 +1,44 @@
 <template>
-  <div>
-    <h1>내가 쓴 리뷰</h1>
+  <div class="mx-5 my-3">
+
+    <v-card flat>
+      <v-card-title>
+        <v-icon color="#fc355d">
+          mdi-file-document
+        </v-icon>
+        <h3 class="mx-1">내가 쓴 리뷰</h3>
+      </v-card-title>
+      
+      <v-progress-linear
+        color="#fc355d"
+        value="45"
+        buffer-value="100"
+        rounded
+      >
+      </v-progress-linear>
+
+      <!-- 리뷰 리스트 -->
+      <v-expansion-panels 
+        class="my-1"
+        accordion
+        multiple
+        hover
+      >
+        <v-expansion-panel
+          v-for="(item,i) in 3"
+          :key="i"
+        >
+          <v-expansion-panel-header>
+            <h4>스타벅스</h4>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content class="mt-1">
+            9시까지 하는 줄 알았는데 저희 동네는 8시까지 하더라구요. 참고하세요!
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+      
+    </v-card>
+
   </div>
 </template>
 
@@ -9,3 +47,15 @@ export default {
   name: "MyPageReview",
 };
 </script>
+
+<style scoped>
+  .v-expansion-panel--active > .v-expansion-panel-header {
+    min-height: 40px;
+  }
+  .v-item-group {
+    position: initial;
+  }
+  .v-card__subtitle, .v-card__text, .v-card__title {
+    padding: 12px;
+  }
+</style>
